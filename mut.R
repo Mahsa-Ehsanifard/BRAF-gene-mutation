6ed44f9748cdb90478654c44ba93086cf01f4553
Mahsa Ehsanifard <167981240+Mahsa-Ehsanifard@users.noreply.github.com>
2024-04-26 23:18:03 +0330

    mut.R

--- mut.R
+++ mut.R
@@ -1,15 +1,11 @@
-### obtaining braf mutation data
+
 BiocManager::install("cBioPortalData")
 library(cBioPortalData)
 data<-data("studiesTable", package = "cBioPortalData")
-skcm <- cBioDataPack("skcm_tcga", ask = FALSE)
 
 cbio <- cBioPortal() #get the information of cbioportal
 study <- getStudies(cbio) # get the studies for choosing my favorite study
 sample <- sampleLists(studyId = "skcm_tcga",cbio) #get the samples for choosing my favorite sample
-sampleid <- samplesInSampleLists(api = cbio,sampleListIds = "skcm_tcga_3way_complete") #get the samples special id which i want
-mol <- molecularProfiles(cbio, studyId = "skcm_tcga") #get the molecul profiles for choosing the molecularprofileID
-molecul <- getGenePanelMolecular(cbio, sampleIds ="skcm_tcga_3way_complete" )
 genepanel <- genePanels(cbio) #get gene panel information
 panel <- getGenePanel(cbio, genePanelId = "IMPACT341")
 
